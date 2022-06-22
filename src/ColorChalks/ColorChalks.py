e0023847f315ac27030b1e5f37c7b5ea775c73e2
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2022-06-22 13:49:37 +0530

    v1.0.1

--- src/ColorChalks/ColorChalks.py
+++ src/ColorChalks/ColorChalks.py
@@ -1,4 +1,4 @@
-class colors: 
+class FCOLORS: 
     Reset = '\u001b[0m'
     Black = '\u001b[30m'
     Red = '\u001b[31m'
@@ -8,5 +8,13 @@ class colors:
     Magenta = '\u001b[35m'
     Cyan = '\u001b[36m'
     White = '\u001b[37m'
+    BrightBlack = '\u001b[30;1m'
+    BrightRed = '\u001b[31;1m'
+    BrightGreen = '\u001b[32;1m'
+    BrightYellow = '\u001b[33;1m'
+    BrightBlue = '\u001b[34;1m'
+    BrightMagenta = '\u001b[35;1m'
+    BrightCyan = '\u001b[36;1m'
+    BrightWhite = '\u001b[37;1m'
 
 
